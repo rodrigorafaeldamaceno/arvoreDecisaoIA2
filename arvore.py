5a28d43c1888bc1cec3298a74dc5d85ea61c5c85
Rodrigo Rafael <rodrigorafaeldamaceno@gmai.com>
2019-08-04 23:56:04 -0300

    alterações na validação de arquivos

--- arvore.py
+++ arvore.py
@@ -8,14 +8,22 @@ def lerArray():
     arquivo = open(sys.argv[1], 'r')
 
     arq = arquivo.read()
-    for token in arq.split():
-        data.append(token)
+    linhas = arq.split('\n')
+    #colunas = len(linhas[0].split(' '))
+
+    #print('Linhas: ', len(linhas), 'Atributos: ', colunas)
+    for i in range(len(linhas)):
+        data.append(linhas[i].split(' '))
+
+    # print(data)
+    '''
     if(sys.argv[1] == 'no3.txt'):
         data = np.array(data).reshape(7, 5)
     elif(sys.argv[1] == 'vote.txt'):
         data = np.array(data).reshape(409, 4)
     elif(sys.argv[1] == 'contact-lenses.txt'):
         data = np.array(data).reshape(25, 4)
+    '''
     arquivo.close()
     return data
 
@@ -25,15 +33,12 @@ def arraysDeSaida(arg):
     arquivo = open(sys.argv[arg], 'r')
 
     arq = arquivo.read()
-    for token in arq.split():
-        data.append(token)
+    linhas = arq.split('\n')
+    # print(linhas)
+    for i in range(len(linhas)-1):
+        data.append(linhas[i].split(' '))
 
-    if(sys.argv[1] == 'no3.txt'):
-        num = len(data)/5
-        data = np.array(data).reshape(int(num), 5)
-    else:
-        num = len(data)/4
-        data = np.array(data).reshape(int(num), 4)
+    # print(data)
 
     arquivo.close()
     return data
@@ -41,28 +46,31 @@ def arraysDeSaida(arg):
 
 def lerArraySaida(data, coluna):
     qtdColunas = np.shape(data)[1]
-    qtdLinhas = np.shape(data)[0]
+    qtdLinhas = np.shape(data)[0]-1
 
     arq_0 = open(sys.argv[2], 'w')
     arq_1 = open(sys.argv[3], 'w')
 
     j = 0
 
-    for i in range(qtdLinhas-1):
+    for i in range(qtdLinhas):
+
         if(data[i+1][coluna] == '1'):
             for j in range(qtdColunas):
                 arq_1.write(data[i+1][j])
-                arq_1.write(' ')
+                if j < qtdColunas-1:
+                    arq_1.write(' ')
 
-            arq_1.writelines('\n')
+            arq_1.write('\n')
 
-            # print(data[i+1][:])
         else:
             for j in range(qtdColunas):
                 arq_0.write(data[i+1][j])
-                arq_0.write(' ')
-            arq_0.writelines('\n')
-            # print(data[i+1][:])
+                if j < qtdColunas-1:
+                    arq_0.write(' ')
+
+            arq_0.write('\n')
+
     arq_1.close()
     arq_0.close()
 
@@ -73,6 +81,8 @@ def erroClassificacao(data, classe):
     negativos = 0
     positivos = 0
 
+    # print(classe)
+
     for i in range(qtdLinhas):
         if(data[i][classe] == '0' or data[i][classe] == '1'):
             if(data[i][classe] == '0'):
@@ -81,8 +91,8 @@ def erroClassificacao(data, classe):
                 positivos += 1
 
     total = negativos+positivos
-    erro = 1 - max((positivos/(negativos + positivos)),
-                   (negativos/(negativos + positivos)))
+    erro = 1 - max((positivos/(total)),
+                   (negativos/(total)))
     return erro
 
 
@@ -118,11 +128,10 @@ def entropy(data, classe):
                 positivos += 1
 
     total = positivos + negativos
-    
-    
+
     positivos += 0.000001
     negativos += 0.000001
-    
+
     entropy = -(positivos/(total) * log2(positivos/(total)) -
                 negativos/(total) * log2(negativos/(total)))
     return abs(entropy)
@@ -135,8 +144,9 @@ def infoGain(impPai, impEsq, impDir, total, qtdEsq, qtdDir):
 
 
 def parte1():
-    '''
-    dataSet = [
+    dataSet = lerArray()
+
+    '''dataSet = [
         ['febre', 'enjoo', 'manchas', 'dores', 'diagnostico'],
         [0, 0, 0, 0, 0],
         [1, 0, 0, 0, 1],
@@ -146,11 +156,15 @@ def parte1():
         [0, 0, 0, 1, 0]]
     '''
 
-    dataSet = lerArray()
     atributo = sys.argv[4]
     impureza = sys.argv[5]
+
+    # print(dataSet)
+
     qtdColunas = np.shape(dataSet)[1]
     qtdLinhas = np.shape(dataSet)[0]
+
+    print(qtdLinhas, qtdColunas)
 
     for i in range(qtdColunas):
         if dataSet[0][i] == atributo:
@@ -226,7 +240,4 @@ def parte2():
 
 if __name__ == '__main__':
 
-    if(sys.argv[1] == 'no3.txt'):
-        parte1()
-    else:
-        parte2()
+    parte1()
